f144f91435141004cf514e2e797bffdacc696802
Søren Roug <soren.roug@eea.europa.eu>
2008-12-10 19:29:24 +0000

    Renaming

--- contrib/odfrenderer/0.10/setup.py
+++ contrib/odfrenderer/0.10/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-PACKAGE = 'OdfRenderer'
+PACKAGE = 'OdfPreview'
 VERSION = '0.1'
 
-setup(name='OdfRenderer',
+setup(name='OdfPreview',
       version='0.1',
-      packages=['odfrenderer'],
+      packages=['odfpreview'],
       author='Soren Roug',
       author_email='soren.roug@eea.europa.eu',
-      description='A plugin for viewing ODF pages as HTML',
-      url='http://trac-hacks.org/wiki/OdfRenderer',
-      entry_points={'trac.plugins': ['odfrenderer.odfrenderer=odfrenderer.odfrenderer']})
+      description='A plugin for viewing ODF documents as HTML',
+      url='http://trac-hacks.org/wiki/OdfPreview',
+      entry_points={'trac.plugins': ['odfpreview.odfpreview=odfpreview.odfpreview']})
