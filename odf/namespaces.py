0e044ddabfa28368aa87f5aa36c635a27585b9eb
Søren Roug <soren.roug@eea.europa.eu>
2018-12-02 13:02:37 +0100

    Preparation for 1.4.1

--- odf/namespaces.py
+++ odf/namespaces.py
@@ -17,7 +17,7 @@
 #
 # Contributor(s):
 #
-__version__ = "1.4.0"
+__version__ = "1.4.1.dev0"
 
 TOOLSVERSION = u"ODFPY/" + __version__
 
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@
 import platform
 from setuptools import setup
 
-version = '1.4.0'
+version = '1.4.1.dev0'
 
 if platform.system() in ('Linux','Unix'):
     man1pages = [('share/man/man1', [
